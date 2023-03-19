da211d786a48d35c58ed2f6e9a689af9a7222c54
Utkarsh Suneela <77976877+utkarshkushi@users.noreply.github.com>
2023-03-19 21:43:15 +0530

    Question 8 solution.

---
+++ Question8.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract ExampleToken is ERC20{
+
+    constructor() ERC20("ExampleTokens", "EXt") {
+        
+    }
+
+    uint256 public TokenPerEth = 10;
+
+    function buyTokens() public payable (
+        require(msg.value > 0, "send enough eth");
+        _mint(msg.sender, msg.value * 10 * 10**18);
+    )
+
+
+}
